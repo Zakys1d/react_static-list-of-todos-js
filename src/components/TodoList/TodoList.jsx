ea1b9529ddb24a21c666a1429a04ce2ec343ed73
Maksym Huk <maksym.h1009@gmail.com>
2026-05-27 21:33:01 +0300

    errors

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,11 @@
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+// eslint-disable-next-line import/no-unresolved
+import { TodoInfo } from './..todos.json';
+
+export const TodoList = ({ todos }) => (
+  <div className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </div>
+);
